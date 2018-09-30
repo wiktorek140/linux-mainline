abf44f05ec5fc213d061c5bac03bcdbf0441f2b9
Jonathan Marek <jonathan@marek.ca>
2018-09-30 13:28:32 -0400

    HACK: drm/msm without iommu

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -432,12 +432,16 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 
 	drm_mode_config_init(ddev);
 
+	ret = msm_init_vram(ddev);
+	if (ret)
+		goto err_msm_uninit;
+
 	/* Bind all our sub-components: */
 	ret = component_bind_all(dev, ddev);
 	if (ret)
 		goto err_destroy_mdss;
 
-	ret = msm_init_vram(ddev);
+	/*ret = msm_init_vram(ddev);
 	if (ret)
 		goto err_msm_uninit;
 
@@ -449,7 +453,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 			goto err_msm_uninit;
 		}
 	}
-	dma_set_max_seg_size(dev, DMA_BIT_MASK(32));
+	dma_set_max_seg_size(dev, DMA_BIT_MASK(32));*/
 
 	msm_gem_shrinker_init(ddev);
 
